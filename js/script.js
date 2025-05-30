4a6784ef45988984b3a01a2ad5d1de738155c7ac
Andrias <andrias.andre@gmail.com>
2025-05-30 20:52:39 +0700

    250530: Upload new products

--- js/script.js
+++ js/script.js
@@ -1,72 +1,86 @@
 // Scroll.js
 
-$(window).on('popstate',function(e){
-	e.preventDefault();
-	var target = window.location.href.split("#")[1];
-	if(target != "" && target!=undefined){
-		$('html, body').stop().animate({'scrollTop': $("#"+target).offset().top}, 500, 'swing', function () {
-			window.location.hash = target;
-		});
-	}
+$(window).on("popstate", function (e) {
+  e.preventDefault();
+  var target = window.location.href.split("#")[1];
+  if (target != "" && target != undefined) {
+    $("html, body")
+      .stop()
+      .animate(
+        { scrollTop: $("#" + target).offset().top },
+        500,
+        "swing",
+        function () {
+          window.location.hash = target;
+        }
+      );
+  }
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
+  if (typeof AOS !== "undefined") {
+    AOS.init({
+      easing: "ease-out-cubic",
+      offset: 50,
+    });
+    setTimeout(function () {
+      if ($(".slick-initialized").length > 0) {
+        AOS.refreshHard();
+      }
+    }, 200);
+  }
 
-	if(typeof(AOS) !== 'undefined'){
-		AOS.init({
-			easing: 'ease-out-cubic',
-			offset: 50
-		});
-		setTimeout(function(){
-			if($(".slick-initialized").length>0){
-				AOS.refreshHard();
-			}
-		},200);
-	}
+  AOS.init();
 
-	AOS.init();
+  $(".nav-link").on("click", function (e) {
+    e.preventDefault();
 
-	$('.nav-link').on('click', function (e) {
-		e.preventDefault();
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
 
-		$('.nav-link').removeClass('active');
-		$(this).addClass('active');
+    $('[id^="image-container"]').hide();
 
-		$('[id^="image-container"]').hide();
+    const target = $(this).attr("href");
+    $(target).show();
 
-		const target = $(this).attr('href');
-		$(target).show();
+    const view = $(target).attr("data-view");
+    if (view === "grid") {
+      $("#gridView")
+        .removeClass("btn-outline-secondary")
+        .addClass("btn-outline-primary");
+      $("#listView")
+        .addClass("btn-outline-secondary")
+        .removeClass("btn-outline-primary");
+    } else {
+      $("#listView")
+        .removeClass("btn-outline-secondary")
+        .addClass("btn-outline-primary");
+      $("#gridView")
+        .addClass("btn-outline-secondary")
+        .removeClass("btn-outline-primary");
+    }
+  });
 
-		const view = $(target).attr("data-view");
-		if (view === "grid") {
-			$("#gridView").removeClass("btn-outline-secondary").addClass("btn-outline-primary");
-			$("#listView").addClass("btn-outline-secondary").removeClass("btn-outline-primary");
-		} else {
-			$("#listView").removeClass("btn-outline-secondary").addClass("btn-outline-primary");
-			$("#gridView").addClass("btn-outline-secondary").removeClass("btn-outline-primary");
-		}
-	});
+  $('[id^="image-container"]').hide();
+  $("#image-container-new").show();
 
-	$('[id^="image-container"]').hide();
-	$('#image-container-new').show();
+  function renderImages(containerId, totalImages, imagePath, altPrefix) {
+    const container = document.getElementById(containerId);
+    if (!container) return;
 
-	function renderImages(containerId, totalImages, imagePath, altPrefix) {
-		const container = document.getElementById(containerId);
-		if (!container) return;
+    const fragment = document.createDocumentFragment();
+    const prefix = altPrefix === "Produk Baru" ? "products-new-250530-" : "";
 
-		const fragment = document.createDocumentFragment();
-		const prefix = altPrefix === "Produk Baru" ? "product-new-" : "";
+    let margin = "";
+    for (let i = 1; i <= totalImages; i++) {
+      const col = document.createElement("div");
+      col.className = "col-12 col-md-4 col-sm-6";
+      col.setAttribute("data-aos-duration", "600");
+      col.setAttribute("data-aos", "fade-down");
+      col.setAttribute("data-aos-delay", "0");
+      margin = i === 1 && $(window).width() < 576 ? "mt-10" : "mt-50";
 
-		let margin = "";
-		for (let i = 1; i <= totalImages; i++) {
-			const col = document.createElement('div');
-			col.className = 'col-12 col-md-4 col-sm-6';
-			col.setAttribute('data-aos-duration', '600');
-			col.setAttribute('data-aos', 'fade-down');
-			col.setAttribute('data-aos-delay', '0');
-			margin = (i === 1 && $(window).width() < 576) ? 'mt-10' : 'mt-50';
-
-			col.innerHTML = `
+      col.innerHTML = `
 					<a href="${imagePath}${prefix}${i}.jpg"
 						class="glightbox link color-main"
 						data-gallery="${altPrefix}"
@@ -78,43 +92,70 @@ $(document).ready(function(){
 					</a>
 				`;
 
-			fragment.appendChild(col);
-		}
+      fragment.appendChild(col);
+    }
 
-		container.appendChild(fragment);
-	}
+    container.appendChild(fragment);
+  }
 
-	renderImages('image-container-new', 27, 'img/New/', 'Produk Baru');
-	renderImages('image-container-frozen-food', 218, 'img/Frozenfood/', 'Produk Frozen Food');
-	renderImages('image-container-snack', 95, 'img/Snack/', 'Produk Snack');
-	renderImages('image-container-bumbu-dapur', 21, 'img/Bumbu/', 'Produk Bumbu Dapur');
-	renderImages('image-container-barang', 29, 'img/Barang/', 'Produk Barang');
-	const glightbox = GLightbox({
-		selector: '.glightbox'
-	});
+  renderImages("image-container-new", 61, "img/New/", "Produk Baru");
+  renderImages(
+    "image-container-frozen-food",
+    230,
+    "img/Frozenfood/",
+    "Produk Frozen Food"
+  );
+  renderImages("image-container-snack", 108, "img/Snack/", "Produk Snack");
+  renderImages(
+    "image-container-bumbu-dapur",
+    22,
+    "img/Bumbu/",
+    "Produk Bumbu Dapur"
+  );
+  renderImages("image-container-barang", 30, "img/Barang/", "Produk Barang");
+  const glightbox = GLightbox({
+    selector: ".glightbox",
+  });
 
-	$('#gridView').on('click', function () {
-		const activeNavLink = $('.nav-link.active').attr('href');
-		$(this).removeClass("btn-outline-secondary").addClass("btn-outline-primary");
-		$("#listView").addClass("btn-outline-secondary").removeClass("btn-outline-primary");
-		$(`${activeNavLink} > div`).each(function () {
-			$(this).attr('class', 'col-6 col-md-4 col-sm-6');
-		});
-		$(`${activeNavLink} > div > a > img`).slice(0, 2).addClass('mt-10').removeClass('mt-50');
-		$(`${activeNavLink}`).attr("data-view", "grid");
-		AOS.refreshHard();
-	});
+  $("#gridView").on("click", function () {
+    const activeNavLink = $(".nav-link.active").attr("href");
+    $(this)
+      .removeClass("btn-outline-secondary")
+      .addClass("btn-outline-primary");
+    $("#listView")
+      .addClass("btn-outline-secondary")
+      .removeClass("btn-outline-primary");
+    $(`${activeNavLink} > div`).each(function () {
+      $(this).attr("class", "col-6 col-md-4 col-sm-6");
+    });
+    $(`${activeNavLink} > div > a > img`)
+      .slice(0, 2)
+      .addClass("mt-10")
+      .removeClass("mt-50");
+    $(`${activeNavLink}`).attr("data-view", "grid");
+    AOS.refreshHard();
+  });
 
-	$('#listView').on('click', function () {
-		const activeNavLink = $('.nav-link.active').attr('href');
-		$(this).removeClass("btn-outline-secondary").addClass("btn-outline-primary");
-		$("#gridView").addClass("btn-outline-secondary").removeClass("btn-outline-primary");
-		$(`${activeNavLink} > div`).each(function () {
-			$(this).attr('class', 'col-12 col-md-4 col-sm-6');
-		});
-		$(`${activeNavLink} > div > a > img`).slice(0, 1).addClass('mt-10').removeClass('mt-50');
-		$(`${activeNavLink} > div > a > img`).slice(1, 2).addClass('mt-50').removeClass('mt-10');
-		$(`${activeNavLink}`).attr("data-view", "list");
-		AOS.refreshHard();
-	});
+  $("#listView").on("click", function () {
+    const activeNavLink = $(".nav-link.active").attr("href");
+    $(this)
+      .removeClass("btn-outline-secondary")
+      .addClass("btn-outline-primary");
+    $("#gridView")
+      .addClass("btn-outline-secondary")
+      .removeClass("btn-outline-primary");
+    $(`${activeNavLink} > div`).each(function () {
+      $(this).attr("class", "col-12 col-md-4 col-sm-6");
+    });
+    $(`${activeNavLink} > div > a > img`)
+      .slice(0, 1)
+      .addClass("mt-10")
+      .removeClass("mt-50");
+    $(`${activeNavLink} > div > a > img`)
+      .slice(1, 2)
+      .addClass("mt-50")
+      .removeClass("mt-10");
+    $(`${activeNavLink}`).attr("data-view", "list");
+    AOS.refreshHard();
+  });
 }); // document.ready end
